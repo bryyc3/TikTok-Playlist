6fb0db33907898e8eca0d0799c683577727bfa46
Bryce King <bryceeking5@gmail.com>
2024-11-18 17:51:57 -0500

    Initial Commit

---
+++ TikTokPlaylist/TikTokPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokPlaylistApp.swift
+//  TikTokPlaylist
+//
+//  Created by Bryce King on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
